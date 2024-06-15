311cf6a15bf59f76474b93164c5254efb698d413
Raj Chowdhury <30806882+Rajchowdhury420@users.noreply.github.com>
2024-06-15 11:27:02 +0530

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://fzl-aws.notion.site/Red-Teaming-Notes-748ed68c885844ec8ba32b6cb4a7df29?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
